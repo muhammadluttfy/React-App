73fffb83eacbe9785ed3a4434f5fa03667855096
muhamammadluttfy <muhammadlutfi.storage@gmail.com>
2022-07-13 14:32:47 +0700

    mendapatkan nilai dari inputan secara dinamis

--- src/components/todo-create/TodoCreate.js
+++ src/components/todo-create/TodoCreate.js
@@ -1,22 +1,32 @@
 import "./TodoCreate.css";
+import { useState } from "react";
 
 const TodoCreate = (props) => {
+  const [getInputTodo, setInputTodo] = useState("");
+
+  const handleInputTodo = (event) => {
+    setInputTodo(event.target.value);
+    // console.log(getInputTodo);
+  };
+
   const handleSubmit = (event) => {
     event.preventDefault();
 
     const newTodo = {
       id: Math.floor(Math.random() * 100) + 1,
-      title: "Learn React",
+      title: getInputTodo,
     };
 
     props.onCreateTodo(newTodo);
+
+    setInputTodo("");
 
     // console.log(newTodo);
   };
 
   return (
     <form className="todo-form" onSubmit={handleSubmit}>
-      <input type="text" />
+      <input type="text" onChange={handleInputTodo} value={getInputTodo} />
       <button type="submit">Add</button>
     </form>
   );
