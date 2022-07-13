b8299211ae705b5f935d016c2767ad607a48ed3a
muhamammadluttfy <muhammadlutfi.storage@gmail.com>
2022-07-13 14:23:27 +0700

    menggunakan state untuk data dinamis

--- src/components/todo/Todo.js
+++ src/components/todo/Todo.js
@@ -1,25 +1,26 @@
 import "./Todo.css";
+import { useState } from "react";
 import TodoList from "../todo-list/TodoList";
 import TodoCreate from "../todo-create/TodoCreate";
 
 const Todo = () => {
-  const todos = [
+  const [getTodos, setTodos] = useState([
     { id: 1, title: "Web Developer" },
     { id: 2, title: "UI UX Designer" },
     { id: 3, title: "Data Analyst" },
     { id: 4, title: "Cyber Security" },
-  ];
+  ]);
 
   const eventCreateTodo = (todo) => {
-    todos.push(todo);
-    console.log(todos);
+    setTodos(getTodos.concat(todo));
+    console.log(getTodos);
   };
 
   return (
     <div>
       <h3>Todo List</h3>
       <TodoCreate onCreateTodo={eventCreateTodo} />
-      <TodoList dataTodos={todos} />
+      <TodoList dataTodos={getTodos} />
     </div>
   );
 };
